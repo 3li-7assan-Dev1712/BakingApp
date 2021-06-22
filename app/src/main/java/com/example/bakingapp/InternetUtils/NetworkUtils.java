2d21eb2dfbd47159c217890f97366dd0ea205b77
Ali Hassan <alihassan17122002@gmail.com>
2021-06-22 12:04:18 -0800

    Prepare the app to connect to the internet

---
+++ app/src/main/java/com/example/bakingapp/InternetUtils/NetworkUtils.java
@@ -0,0 +1,29 @@
+package com.example.bakingapp.InternetUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if (hasInput) {
+                response = scanner.next();
+            }
+            scanner.close();
+            return response;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
