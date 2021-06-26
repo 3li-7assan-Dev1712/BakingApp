4efb342d23f938b6b92a5ec8fc1b33cffbdf3ef0
Ali Hassan <alihassan17122002@gmail.com>
2021-06-26 08:57:34 -0800

    Add the first ViewModel.

---
+++ app/src/main/java/com/example/bakingapp/ViewModels/RecipesViewModel.java
@@ -0,0 +1,27 @@
+package com.example.bakingapp.ViewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.bakingapp.Entries.RecipeEntry;
+import com.example.bakingapp.database.BakingDatabase;
+
+import java.util.List;
+
+public class RecipesViewModel extends AndroidViewModel {
+
+
+    private LiveData<List<RecipeEntry>> recipeEntryLiveData;
+    public RecipesViewModel(@NonNull Application application) {
+        super(application);
+        BakingDatabase bakingDatabase = BakingDatabase.getsInstance(this.getApplication());
+        recipeEntryLiveData = bakingDatabase.recipeDao().loadAllRecipes();
+    }
+
+    public LiveData<List<RecipeEntry>> getRecipeEntryLiveData() {
+        return recipeEntryLiveData;
+    }
+}
